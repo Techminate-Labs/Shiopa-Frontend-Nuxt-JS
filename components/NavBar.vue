df7fead46d2ad7de5f94e97c78ee85de9566c5ad
liefwerk <natjageni@gmail.com>
2021-06-03 14:12:56 +0200

    added basic compoennts and a few pages, need to try handling layouts next

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-row flex-nowrap bg-gray-100 py-3 px-8 justify-between">
+    <span class="mr-2 font-bold text-3xl">Shiopa</span>
+    <ul class="self-center">
+      <li>Page</li>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class NavBar extends Vue {
+
+}
+</script>
--- pages/categories.vue
+++ pages/categories.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="container">
     <NavBar />
-    <Hero />
-    <Categories />
-    <Footer />
   </div>
 </template>
 
