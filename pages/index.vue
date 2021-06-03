9806dd410dd91070981fa5b5de33d44855c6b228
liefwerk <natjageni@gmail.com>
2021-06-03 13:09:00 +0200

    added upstream master, this push serves as a confirmation

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,10 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
       <h1 class="title">
         shiopa
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <p class="text-lg">Let's get started!</p>
     </div>
   </div>
 </template>
