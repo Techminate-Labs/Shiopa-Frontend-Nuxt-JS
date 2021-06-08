55b683b3af10b25f1eef96be32152102f361dc0e
SazidAhmed <sazidahmed.official@gmail.com>
2021-06-09 04:36:33 +0600

    Key added in v-for

--- components/Products.vue
+++ components/Products.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto xl:px-16 my-16">
     <h2 class="text-center mb-8 text-3xl">Products</h2>
     <div class="flex flex-row flex-wrap justify-center md:justify-start">
-      <div v-for="product in products" class="max-w-max md:w-1/2 lg:w-1/3 xl:w-1/4 block relative">
+      <div v-for="product in products" :key="product.id" class="max-w-max md:w-1/2 lg:w-1/3 xl:w-1/4 block relative">
         <img
           :src="'/placeholders/' + product.image"
         />
