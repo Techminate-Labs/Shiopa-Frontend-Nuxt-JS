41a9e68768f1b8b4b54f92aedcbb7be15cfa548e
liefwerk <natjageni@gmail.com>
2021-07-30 14:37:37 +0200

    added GraphCard

---
+++ components/administration/dashboard/cards/GraphCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col py-4 px-4 bg-white rounded shadow-md">
+    <p class="text-lg">Total Orders</p>
+    <p class="text-2xl text-blue-900">$1250</p>
+    <p class="text-md uppercase text-gray-400">Monday</p>
+    <div class="flex flex-col">
+      <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-900 self-end h-14 w-14" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+</svg>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Emit } from 'nuxt-property-decorator'
+
+@Component
+export default class GraphCard extends Vue {}
+</script>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="text-gray-600 m-12">
-    <h1 class="text-4xl">Dashboard</h1>
-    <p>Breadcrumb</p>
-    <div class="my-8 grid grid-flow-col grid-cols-3 gap-6">
-      <IconCard />
-      <IconCard />
-      <IconCard />
+    <div>
+      <h1 class="text-4xl">Dashboard</h1>
+      <p>Breadcrumb</p>
+    </div>
+    <div class="grid grid-cols-2 gap-7">
+      <div class="my-8 grid grid-flow-col grid-cols-3 gap-6">
+        <IconCard />
+        <IconCard />
+        <IconCard />
+      </div>
+      <div class="my-8 grid grid-flow-col grid-cols-3 gap-6">
+        <GraphCard />
+        <GraphCard />
+        <GraphCard />
+      </div>
     </div>
   </div>
 </template>
@@ -13,9 +22,11 @@
 <script lang="ts">
 import Vue from 'vue'
 import IconCard from '@/components/administration/dashboard/cards/IconCard.vue'
+import GraphCard from '@/components/administration/dashboard/cards/GraphCard.vue'
 
 export default Vue.extend({
   components: {
+    GraphCard,
     IconCard,
   },
   layout (context) {
