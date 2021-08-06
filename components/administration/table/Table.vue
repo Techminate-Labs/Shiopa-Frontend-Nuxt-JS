b236432872bbb0cce21883cb961527f07dc6e732
liefwerk <natjageni@gmail.com>
2021-08-06 15:30:03 +0200

    Fixed minor isues with pagination and added columns as props and v-for loop

--- components/administration/table/Table.vue
+++ components/administration/table/Table.vue
@@ -6,7 +6,7 @@
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
               <tr>
-                <th @click="sort(column.key)" v-for="(column, index) in columns" :key="index" scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th @click="sort(column.attribute)" v-for="(column, index) in columns" :key="index" scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   {{ column.name }}
                 </th>
                 <th scope="col" class="relative px-6 py-3">
@@ -15,35 +15,15 @@
               </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
-              <tr v-for="(item, index) in sortedItems" :key="index">
-                <td class="px-6 py-4 whitespace-nowrap">
+              <tr v-for="(item, i) in sortedItems" :key="i">
+                <td v-for="(textColumn, j) in textColumns" :key="j" class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex-shrink-0">
+                    {{ item[textColumn.attribute] }}
+                  </div>
+                </td>
+                <td v-for="(image, k) in imageColumn" :key="k" class="px-6 py-4 whitespace-nowrap">
                   <div class="flex-shrink-0 h-10 w-10">
-                    {{ item.id }}
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="flex-shrink-0 h-10 w-10">
-                    <img class="h-10 w-10 rounded-full" :src="item.image" alt="" />
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm font-medium text-gray-900">
-                    {{ item.name }}
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm text-gray-500">
-                    {{ item.slug }}
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm text-gray-500">
-                    {{ item.parentCategory }}
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm text-gray-500">
-                    {{ item.numberOfProducts }}
+                    <img class="h-10 w-10 rounded-full" :src="item[image.attribute]" alt="" />
                   </div>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
@@ -84,7 +64,8 @@
               </div>
               <div v-if="notEnoughPages">
                 <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-                  <a 
+                  <a
+                    @click="changeToPreviousPage"
                     href="#" 
                     class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                     <span>Previous</span>
@@ -98,7 +79,10 @@
                     class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
                     {{ number }}
                   </a>
-                  <a href="#" class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                  <a
+                    @click="changeToNextPage"
+                    href="#"
+                    class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                     <span>Next</span>
                   </a>
                 </nav>
@@ -139,7 +123,7 @@ export default class Table extends Vue {
 
   public activeItem: number | null = null 
 
-  public onChange(): void {
+  onChange(): void {
     this.sortedItems
     this.itemsInPage
     this.displayPages
@@ -149,7 +133,7 @@ export default class Table extends Vue {
     this.lastPage = Math.ceil(this.totalItems / this.maxItemsPerPage)
     const totalPages = this.lastPage;
     let currentPage: any = this.currentPage;
-    if ([1, 2, 3].includes(currentPage)) currentPage = 3;
+    if ([1, 2, 3, 4].includes(currentPage)) currentPage = 3;
     else if ([totalPages - 1, totalPages].includes(currentPage)) currentPage = Math.max(0, totalPages - Math.trunc(5 / 2));
 
     if (totalPages < 5){
@@ -162,6 +146,28 @@ export default class Table extends Vue {
   changeItemsInPage(num: number): void {
     this.currentPage = num
     this.activeItem = num
+  }
+
+  changeToPreviousPage(): void {
+    if (this.currentPage && this.activeItem)
+      if (this.currentPage > 1) {
+        this.currentPage -= 1
+        this.activeItem -= 1
+      } else {
+        this.currentPage = 1
+        this.activeItem = 1
+      }
+  }
+
+  changeToNextPage(): void {
+    if (this.currentPage && this.activeItem)
+      if (this.currentPage < this.lastPage) {
+        this.currentPage += 1
+        this.activeItem += 1
+      } else {
+        this.currentPage = this.lastPage
+        this.activeItem = this.lastPage
+      }
   }
 
   sort(s: string): void {
@@ -186,6 +192,14 @@ export default class Table extends Vue {
       if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
       return 0;
     });
+  }
+
+  get textColumns(): any[] {
+    return this.columns.filter((c: any) => c.attribute !== 'image' )
+  }
+
+  get imageColumn(): any[] {
+     return this.columns.filter((c: any) => c.attribute === 'image' )
   }
 
 }
--- pages/dashboard/categories/index.vue
+++ pages/dashboard/categories/index.vue
@@ -1,12 +1,248 @@
 <template>
-  <div>
-      <h1>Categories</h1>
+  <div class="p-8 w-full md:w-5/6 lg:w-10/12 xl:w-11/12">
+    <h1 class="text-4xl mb-4">Categories</h1>
+    <Table :items="this.items" :columns="this.columns"/>
   </div>
 </template>
 
 <script>
+import Table from '@/components/administration/table/Table.vue'
+
 export default {
   layout: 'dashboard',
+  components: {
+    Table
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Naga Burger',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 17,
+        },
+        {
+          id: 2,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 10,
+        },
+        {
+          id: 3,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Patty Burger',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 2,
+        },
+        {
+          id: 4,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Damn Good Burger',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 5,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Super Duper Damn Good Burger',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 6,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That is So Good It\'s On The next Page',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 7,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That is So Good It\'s On The next Page',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 8,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That is So Good It\'s On The next Page',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 9,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That is So Good It\'s On The next Page',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 10,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That is So Good It\'s On The next Page',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 11,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 12,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 13,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 14,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 15,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 16,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 17,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 18,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 19,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 20,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Last Burger That You Can\'t Even ?',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 21,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Not The Last Burger That You Can\'t Even',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+        {
+          id: 22,
+          image:
+            'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80',
+          name: 'Last Burger That You Can\'t Even !!!',
+          slug: 'lorem ipsum',
+          parentCategory: 'lorem ipsum',
+          numberOfProducts: 108,
+        },
+      ],
+      columns: [
+        {
+          name: '#',
+          attribute: 'id'
+        },
+        {
+          name: 'name',
+          attribute: 'name'
+        },
+        {
+          name: 'slug',
+          attribute: 'slug'
+        },
+        {
+          name: 'Parent Category',
+          attribute: 'parentCategory'
+        },
+        {
+          name: 'number of products',
+          attribute: 'numberOfProducts'
+        },
+        {
+          name: 'image',
+          attribute: 'image'
+        },
+      ]
+    }
+  }
 
 }
 </script>
