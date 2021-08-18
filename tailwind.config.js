bcbd74bc283e2c1f4ff32267ef1dbd4e60ce2c1e
SazidAhmed <sazidahmed.official@gmail.com>
2021-08-18 23:20:43 +0600

    tailwind config added

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary: "#042235",
+        secondary: "#042235",
+      },
+      fontFamily: {
+        serif: ['Quando'],
+        body: ['Roboto'],
+      },
+      fontSize: {
+        titlexl: ['2.986em'],
+        titlelg: ['2.074em'],
+        titlemd: ['1.728em'],
+        subtitle: ['1.2em'],
+        intxt: ['1em'],
+        intable: ['0.833em'],
+        inbtn: ['1.2em'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
