f903c6dc099b5b03f00c1689c3dd37e04fa1db19
afrinsarker <afrinsarker91@gmail.com>
2022-01-30 09:31:47 +0600

    updated home page

--- components/storefront/products/categories/SingleCategories.vue
+++ components/storefront/products/categories/SingleCategories.vue
@@ -26,107 +26,11 @@
         </div>
         <!-----End 2nd part---->
       </div>
-      <!--new part--->
-      <div class="px-5 py-5 pt-10 grid gap-6 mb-8 md:grid-cols-2 lg:grid-cols-4">
-        <!-- Card 1 -->
-        <div class="flex items-center p-4" v-for="(homeCategory, index) in homeCategories" :key="index">
-          <div class="p-6 mr-4 bg-gray-200 text-gray-800 hover:bg-yellow-600 rounded-full">
-            <img
-              :src="'/placeholders/' +  homeCategory.image"
-              :alt="homeCategory.image_alt"
-            />
-          </div>
-          <div>
-            <h1 class="home-h1">{{homeCategory.name}}</h1>
-            <p class="text-sm font-normal text-gray-800">{{homeCategory.description}}</p>
-          </div>
-        </div>
-      </div>
-      <!---new part 2--->
-        <div class="px-0 py-5">
-          <div class="lg:w-4/5 mx-auto flex flex-wrap">
-            <img 
-              src="https://template.hasthemes.com/urdan/urdan/assets/images/product/single-product.png"
-              alt="homeCategory.image_alt"
-              class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded "
-            />
-            <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-              <h1 class="text-3xl title-font font-medium mb-1 text-yellow-700">Modern Chair</h1>
-              <span class="span-title title-font">$58.00</span>
-              <p class="leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempo incididunt ut labore et dolore mt aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-              <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                <div class="flex">
-                  <span class="mr-3">Color</span>
-                  <button class="border-2 bg-pink-300 w-6 h-6 focus:outline-none"></button>
-                  <button class="border-2 ml-1 bg-yellow-700 w-6 h-6 focus:outline-none"></button>
-                  <button class="border-2 border-gray-300 ml-1 bg-indigo-500 w-6 h-6 focus:outline-none"></button>
-                </div>
-              </div>
-              <div class="flex justify-space-around">
-                <div class="flex flex-row flex-nowrap justify-start border border-gray-200 mr-2">
-                  <div  class="svg-class ">
-                    <svg class="fill-current text-gray-600 h-3 w-3" viewBox="0 0 448 512">
-                      <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-                    </svg>
-                  </div>
-                  <input class="mx-2 border text-center w-8" type="number" min="0">
-                  <div class="svg-class ">
-                    <svg class="fill-current text-gray-600 h-3 w-3" viewBox="0 0 448 512">
-                      <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-                    </svg>
-                  </div>
-                </div>
-                <button class="btnclass">Shop now</button>
-              </div>
-            </div>
-          </div>
-        </div>
     </div>
 </template>
 
-<script lang="ts">
-import { Prop, Component, Vue } from 'nuxt-property-decorator'
+<script >
 
- interface Cart {
-    homeCategory_id: number | null,
-    name: string | null,
-    slug: string | null,
-    description: string | null,
-    image: string | null,
-    img: string | null
-    
-
-  }
-
-  interface homeCategory {
-    additional_info: string,
-    available_to_purchase: boolean,
-    description: string,  
-    discount_price: string,
-    get_absolute_url: string,
-    get_thumbnail: string,
-    id: number,
-    images: [],
-    name: string,
-    image: string,
-    img: string,
-    slug: string
-  }
-
-  @Component
-  export default class homeCategories extends Vue{
-
-    @Prop({ required: true }) readonly homeCategories!: homeCategory[]
-    public localData: Cart = {
-    homeCategory_id: null,
-    name: null,
-    description: null,
-    image: null,
-    img: null,
-    slug: null,
-  }
-
-  }
 </script>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,17 @@
 	<div>
 		<Slider :sliders="sliders" />
 		<div class="container mx-auto">
-			<DealOfTheDay />
+			
 			<Categories :categories="categories" class="mt-8 mb-20" />
-
-			<h2>Category 1</h2>
+            <DealOfTheDay />
 			<CategoryProducts :categoryProducts="firstCategory" class="mb-20" />
+			<SingleCategories class="mt-8 mb-20" />
+			<ModernCategories class="mt-8 mb-20" />
+			<CategoryDining class="mb-20" />
 			<h2>Category 2</h2>
 			<CategoryProducts :categoryProducts="secondCategory" class="mb-20" />
+			<h2>Product Images</h2>
+			<ImageCategories class="mb-20" />
 			<h2>Latest Products</h2>
 			<CategoryProducts :categoryProducts="latestProducts" class="mb-20" />
 		</div>
@@ -21,13 +25,21 @@ import Vue from 'vue'
 // components
 import Slider from '@/components/storefront/products/Slider.vue'
 import Categories from '@/components/storefront/products/categories/Categories.vue'
+import SingleCategories from '@/components/storefront/products/categories/SingleCategories.vue'
+import ModernCategories from '@/components/storefront/products/categories/ModernCategories.vue'
+import CategoryDining from '@/components/storefront/products/categories//dining/CategoryDining.vue'
+import ImageCategories from '@/components/storefront/products/categories/ImageCategories.vue'
 import CategoryProducts from '@/components/storefront/products/categories/CategoryProducts.vue'
 import DealOfTheDay from '@/components/storefront/products/DealOftheDay.vue'
 
 export default Vue.extend({
 	components: {
 		Categories,
+		SingleCategories,
 		Slider,
+		ModernCategories,
+		CategoryDining,
+		ImageCategories,
 		CategoryProducts,
 		DealOfTheDay
 	},
